2ba785534dda6007c6a0c99a5703d23fa331249c
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-12 19:35:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function makeid(l) {
   // write your code here
+	var res="";
+	var var_list="ABCDEFGHIJKLMNOPQRSTVUWYZ0987654321qwertyuioplkjhgfdsazxcvbnm";
+	for(let i=0; i<l; i++){
+		res+=var_list.charAt(
+			Math.floor(
+				Math.random*var_list.length;
+			)
+		)
+		
+	}
+	return res;
 }
 
-// Do not change the code below.
+// Do not change the code belo
 const l = prompt("Enter a number.");
 alert(makeid(l));
